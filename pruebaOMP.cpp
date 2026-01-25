09d5b73d0c63489fdd5f7bd22f3d0694e373fa69
Asael Meza <asa_1998@hotmail.com>
2026-01-25 16:21:22 -0600

    Se comenta el código

--- pruebaOMP.cpp
+++ pruebaOMP.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-#include <omp.h> // Librería necesaria para el uso de funciones y directivas de OpenMP
+#include <omp.h> // Uso de OpenMP
 
-// Definición de constantes para facilitar el mantenimiento del código
-#define N 1000      // Cantidad total de elementos en los arreglos
-#define chunk 100   // Tamaño de las porciones (chunks) de trabajo asignadas a cada hilo
-#define mostrar 100  // Cantidad de elementos a mostrar en consola
+// Constantes 
+#define N 1000      // Elementos en los arreglos
+#define chunk 100   // Tamaño de los chunks asignados a cada hilo
+#define mostrar 100  // Elementos a mostrar en consola
 
 void imprimeArreglo(float* d);
 
@@ -12,15 +12,15 @@ int main()
 {
     std::cout << "Sumando Arreglos en Paralelo!\n";
 
-    // Declaración de arreglos y variable de control para el ciclo
+    // Arreglos y variable de control para el ciclo
     float a[N], b[N], c[N];
     int i;
 
-    //// Inicialización de los arreglos de forma secuencial
+    //// Arreglos de forma secuencial
     for (i = 0; i < N; i++)
     {
-        a[i] = i * 1.5f; // Rellenamos 'a' con valores flotantes
-        b[i] = (i + 2) * 2.0f; // Rellenamos 'b' con valores diferentes
+        a[i] = i * 1.5f; // Rellenamos 'a' con flotantes
+        b[i] = (i + 2) * 2.0f; // Rellenamos 'b'
     }
 
     int pedazos = chunk;
@@ -41,7 +41,7 @@ int main()
         c[i] = a[i] + b[i];
     }
 
-    // Impresión de resultados usando la función auxiliar
+    // Impresión de resultados
     std::cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo a: " << std::endl;
     imprimeArreglo(a);
 
@@ -54,7 +54,7 @@ int main()
     return 0;
 }
 
-// Función para imprimir una cantidad limitada de elementos de un arreglo
+// Imprime una cantidad limitada de elementos de un arreglo
 void imprimeArreglo(float* d)
 {
     for (int x = 0; x < mostrar; x++)
